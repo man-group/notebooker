a4708ca213408c5b650302beb1d8fc356b12c880
jbannister <Jon.Bannister@man.com>
2022-05-04 15:11:43 +0100

    Bugfix for prometheus success/error reporting.

--- tests/integration/test_report_hunter.py
+++ tests/integration/test_report_hunter.py
@@ -9,7 +9,8 @@ from notebooker.constants import JobStatus, NotebookResultComplete, NotebookResu
 from notebooker.serialization.serialization import initialize_serializer_from_config
 from notebooker.utils.caching import get_report_cache
 from notebooker.utils.filesystem import initialise_base_dirs
-from notebooker.web.report_hunter import _report_hunter
+from notebooker.utils.results import _get_job_results
+from notebooker.web.report_hunter import _report_hunter, LRUSet
 
 
 @pytest.fixture(autouse=True)
@@ -124,6 +125,45 @@ def test_report_hunter_timeout(bson_library, status, time_later, should_timeout,
         assert get_report_cache(report_name, job_id, cache_dir=webapp_config.CACHE_DIR) == expected
 
 
+@mock.patch("notebooker.web.routes.prometheus.record_failed_report")
+def test_prometheus_logging_when_cache_is_already_updated(record_failed_report, bson_library, webapp_config):
+    job_id = str(uuid.uuid4())
+    report_name = str(uuid.uuid4())
+    serializer = initialize_serializer_from_config(webapp_config)
+
+    with freezegun.freeze_time(datetime.datetime(2018, 1, 12, 2, 30)):
+        serializer.save_check_stub(job_id, report_name, status=JobStatus.SUBMITTED)
+        _report_hunter(webapp_config=webapp_config, run_once=True)
+        expected = NotebookResultPending(
+            job_id=job_id,
+            report_name=report_name,
+            report_title=report_name,
+            status=JobStatus.SUBMITTED,
+            update_time=datetime.datetime(2018, 1, 12, 2, 30),
+            job_start_time=datetime.datetime(2018, 1, 12, 2, 30),
+        )
+        assert get_report_cache(report_name, job_id, cache_dir=webapp_config.CACHE_DIR) == expected
+
+    with freezegun.freeze_time(datetime.datetime(2018, 1, 12, 5, 37)):
+        expected = NotebookResultError(
+            job_id=job_id,
+            report_name=report_name,
+            report_title=report_name,
+            status=JobStatus.ERROR,
+            update_time=datetime.datetime(2018, 1, 12, 5, 37),
+            job_start_time=datetime.datetime(2018, 1, 12, 2, 30),
+            error_info="This crashed!",
+        )
+        serializer.save_check_result(expected)
+
+        # Now someone checks the result on the webapp, which updates the cache...!
+        _get_job_results(job_id, report_name, serializer=serializer, ignore_cache=True)
+
+        _report_hunter(webapp_config=webapp_config, run_once=True)
+        assert get_report_cache(report_name, job_id, cache_dir=webapp_config.CACHE_DIR) == expected
+        record_failed_report.assert_called_once_with(report_name, report_name)
+
+
 def test_report_hunter_pending_to_done(bson_library, webapp_config):
     job_id = str(uuid.uuid4())
     report_name = str(uuid.uuid4())
@@ -227,3 +267,20 @@ def test_prometheus_logging_in_report_hunter_no_prometheus_success(
         _report_hunter(webapp_config=webapp_config, run_once=True)
         assert get_report_cache(report_name, job_id, cache_dir=webapp_config.CACHE_DIR) == expected
         record_successful_report.assert_called_once_with(report_name, report_name)
+
+
+def test_lru_set():
+    s = LRUSet(max_size=10)
+    assert len(s) == 0
+    for i in range(100):
+        s.add(i)
+    assert len(s) == 10
+    for i in range(90):
+        assert i not in s
+    for i in range(90, 100):
+        assert i in s
+    s.remove(99)
+    assert 99 not in s
+    assert len(s) == 9
+    for set_item, range_item in zip(s, range(90, 99)):
+        assert set_item == range_item
