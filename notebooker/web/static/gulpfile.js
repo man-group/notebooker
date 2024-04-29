76f7b259191ad6a11c33fe342255fbddc97a1e83
jbannister <Jon.Bannister@man.com>
2024-04-29 10:02:17 +0100

    Fix bug where folders with whitespace were unable to have their results listed. Fixes #173. Create unit tests for index.js using Jest. Small refactor of index.js to use JS standards & apply Prettier.

--- notebooker/web/static/gulpfile.js
+++ notebooker/web/static/gulpfile.js
@@ -2,34 +2,29 @@
  *           Set-up
  *******************************/
 
-var
-    gulp   = require('gulp'),
-
+const gulp = require("gulp"),
     // read user config to know what task to load
-    config = require('./tasks/config/user')
-;
-
-
+    config = require("./tasks/config/user");
 /*******************************
  *            Tasks
  *******************************/
 
-require('./tasks/collections/build')(gulp);
-require('./tasks/collections/various')(gulp);
-require('./tasks/collections/install')(gulp);
+require("./tasks/collections/build")(gulp);
+require("./tasks/collections/various")(gulp);
+require("./tasks/collections/install")(gulp);
 
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("watch"));
 
 /*--------------
       Docs
 ---------------*/
 
-require('./tasks/collections/docs')(gulp);
+require("./tasks/collections/docs")(gulp);
 
 /*--------------
       RTL
 ---------------*/
 
 if (config.rtl) {
-    require('./tasks/collections/rtl')(gulp);
+    require("./tasks/collections/rtl")(gulp);
 }
--- notebooker/web/static/notebooker/index.js
+++ notebooker/web/static/notebooker/index.js
@@ -1,132 +1,133 @@
-
-function getCurrentFolder(){
-    if (window.location.pathname.startsWith("/folder/")){
-        currentFolder = window.location.pathname.substring("/folder/".length) + "/"
-    } else {
-        currentFolder = ""
+const getCurrentFolder = () => {
+    let currentFolder = "";
+    if (window.location.pathname.startsWith("/folder/")) {
+        currentFolder = window.location.pathname.substring("/folder/".length) + "/";
     }
-    return currentFolder
-}
+    return decodeURI(currentFolder);
+};
 
-function updateNavigationPanel(currentFolder){
-    let $folderNavigationPanel = $('#folderNavigationPanel');
-    $folderNavigationPanel.empty()
-    
-    if (currentFolder != "") {
-        $folderNavigationPanel.append('<a href="/">Start</a>')
-        parts = currentFolder.split("/")
-        folderSoFar = ""
-        for(let idx in parts){
-            part = parts[idx]
-            if(part.length > 0){
-                folderSoFar = folderSoFar + "/" + part
-                if(idx == parts.length - 2){
-                    $folderNavigationPanel.append('&nbsp;&gt;&nbsp;<span>' + part + "</span>")
-                }else{
-                    $folderNavigationPanel.append('&nbsp;&gt;&nbsp;<a href="/folder' + folderSoFar + '">' + part + " </a>")
+const updateNavigationPanel = (currentFolder) => {
+    const folderNavigationPanel = $("#folderNavigationPanel");
+    folderNavigationPanel.empty();
+
+    if (currentFolder !== "") {
+        folderNavigationPanel.append('<a href="/">Start</a>');
+        const parts = currentFolder.split("/");
+        let folderSoFar = "";
+        parts.forEach((part, idx) => {
+            if (part.length > 0) {
+                folderSoFar = `${folderSoFar}/${part}`;
+                if (idx === parts.length - 2) {
+                    folderNavigationPanel.append(`&nbsp;&gt;&nbsp;<span>${part}</span>`);
+                } else {
+                    folderNavigationPanel.append(`&nbsp;&gt;&nbsp;<a href="/folder${folderSoFar}">${part} </a>`);
                 }
-                
             }
-        }
+        });
     }
-}
+};
 
-
-function entryAfterLevel(path, level){
-    while(level > 0) {
-        path = path.substring(path.indexOf('/') + 1)
-        level = level - 1
+const entryAfterLevel = (path, level) => {
+    while (level > 0) {
+        path = path.substring(path.indexOf("/") + 1);
+        level = level - 1;
     }
-    return path
-}
+    return path;
+};
 
-function updateContents(currentFolder, entries){
-    let $cardContainer = $('#cardContainer');
+const updateContents = (currentFolder, entries) => {
+    const $cardContainer = $("#cardContainer");
     $cardContainer.empty();
-    subfoldersInfo = {}
-    reportParts = []
-    for (let report in entries) {
-        report_data = entries[report]
-        report_path = report_data.report_name
-        if(!report_path.startsWith(currentFolder)){
+    const subfoldersInfo = {};
+    const reportParts = [];
+    for (const report in entries) {
+        const report_data = entries[report];
+        const report_path = report_data.report_name;
+        if (!report_path.startsWith(currentFolder)) {
             continue;
         }
-        remainingPath = report_path.substring(currentFolder.length)
-        if(remainingPath.includes('/')) {
+        const remainingPath = report_path.substring(currentFolder.length);
+        if (remainingPath.includes("/")) {
             // it is a folder
-            subfolderPathName = remainingPath.substring(0, remainingPath.indexOf('/'))
-            subfolderPath = currentFolder + subfolderPathName
+            const subfolderPathName = remainingPath.substring(0, remainingPath.indexOf("/"));
+            const subfolderPath = currentFolder + subfolderPathName;
+            let info = subfoldersInfo[subfolderPath];
             if (subfolderPath in subfoldersInfo) {
-                info.reportCount = info.reportCount + 1
-            } else{
-                info = {}
-                info.pathName = subfolderPathName
-                info.path = subfolderPath
-                info.reportCount = 1
-                subfoldersInfo[subfolderPath] = info
+                info.reportCount = info.reportCount + 1;
+            } else {
+                let info = {};
+                info.pathName = subfolderPathName;
+                info.path = subfolderPath;
+                info.reportCount = 1;
+                subfoldersInfo[subfolderPath] = info;
             }
-        }else{
-            level = currentFolder.split('/').length - 1
-            displayName = entryAfterLevel(report, level)
-            let stats = entries[report];
-            reportParts.push('<a class="ui card" href="/result_listing/' + stats.report_name + '">' +
-                '  <div class="content">' +
-                '    <h1>' + displayName + '</h1>\n' +
-                '    <div class="meta">\n' +
-                '      <span class="date">Last ran ' + stats.time_diff + ' ago</span>\n' +
-                '    </div>' +
-                '    <span>\n' +
-                stats.count +
-                '        Runs\n' +
-                '    </span>' +
-                '<br/>' +
-                '    <span>\n' +
-                stats.scheduler_runs +
-                '        Scheduler Runs\n' +
-                '    </span>' +
-                '  </div>' +
-                '  <div class="extra content">' +
-                '      <span>Original report name: ' + stats.report_name + '</span>\n' +
-                '  </div>' +
-                '</a>');
+        } else {
+            const level = currentFolder.split("/").length - 1;
+            const displayName = entryAfterLevel(report, level);
+            const stats = entries[report];
+            reportParts.push(
+                `<a class="ui card" href="/result_listing/${stats.report_name}">` +
+                    '  <div class="content">' +
+                    `    <h1>${displayName}</h1>\n` +
+                    '    <div class="meta">\n' +
+                    `      <span class="date">Last ran ${stats.time_diff} ago</span>\n` +
+                    "    </div>" +
+                    "    <span>\n" +
+                    `${stats.count} Runs\n` +
+                    "    </span>" +
+                    "<br/>" +
+                    "    <span>\n" +
+                    `${stats.scheduler_runs} Scheduler Runs\n` +
+                    "    </span>" +
+                    "  </div>" +
+                    '  <div class="extra content">' +
+                    `      <span>Original report name: ${stats.report_name}</span>\n` +
+                    "  </div>" +
+                    "</a>"
+            );
         }
-        
-        
     }
     // first add all folders
     for (let subfolder in subfoldersInfo) {
-        info = subfoldersInfo[subfolder]
+        let info = subfoldersInfo[subfolder];
         $cardContainer.append(
-            '<a class="ui card folder" href="/folder/' + info.path + '">' +
-            '  <div class="content">' +
-            '    <h1><i class="fa-solid fa-folder fa-xs"></i> ' + info.pathName + '</h1>\n' +
-            ' <span>Reports: ' + info.reportCount + '</span>' + 
-            '  </div>' +
-            '</a>');
+            `<a class="ui card folder" href="/folder/${info.path}">` +
+                '  <div class="content">' +
+                `    <h1><i class="fa-solid fa-folder fa-xs"></i>${info.pathName}</h1>\n` +
+                `    <span>Reports: ${info.reportCount}</span>` +
+                "  </div>" +
+                "</a>"
+        );
     }
     // only then add individual items
     for (let idx in reportParts) {
-        $cardContainer.append(reportParts[idx])
+        $cardContainer.append(reportParts[idx]);
     }
-}
-
-
-load_data = () => {
-    $.ajax({
-        url: `/core/get_all_templates_with_results/folder/`.concat(getCurrentFolder()),
-        dataType: 'json',
-        success: (result) => {
-            currentFolder = getCurrentFolder()
-            updateNavigationPanel(currentFolder)
-            updateContents(currentFolder, result)
-        },
-        error: (jqXHR, textStatus, errorThrown) => {
-            $('#failedLoad').fadeIn();
-        },
-    });
 };
 
+const load_data = async () => {
+    const currentFolder = getCurrentFolder();
+    try {
+        const response = await fetch(`/core/get_all_templates_with_results/folder/`.concat(currentFolder));
+        const result = await response.json();
+        updateNavigationPanel(currentFolder);
+        updateContents(currentFolder, result);
+    } catch (error) {
+        $("#failedLoad").fadeIn();
+    }
+};
 
-$(document).ready(() => {
-    load_data();
+$(document).ready(async () => {
+    await load_data();
 });
+
+try {
+    module.exports = {
+        getCurrentFolder,
+        updateNavigationPanel,
+        updateContents,
+        load_data,
+    };
+} catch (e) {
+    // do nothing
+}
