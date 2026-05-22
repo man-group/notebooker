7b6f76f56a21febd16be9800bc50289d0972bd62
Jon Bannister <jon.bannister@man.com>
2026-05-22 18:00:49 +0100

    Bump version to 0.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Man Group Quant Tech"
 author = "Man Group Quant Tech"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.3"
+release = "0.8.0"
 
 
 # -- General configuration ---------------------------------------------------
